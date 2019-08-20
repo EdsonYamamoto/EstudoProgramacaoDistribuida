85cd62941b9a8d690a90c6b1118d2096b870bbcb
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2019-08-20 16:34:44 -0300

    Adicionado sistema de log, armazenado no bin quando executado a aplicação.

--- ProjetoChatProgramDistrubuida/Program.cs
+++ ProjetoChatProgramDistrubuida/Program.cs
@@ -12,38 +12,50 @@ namespace ProjetoChatProgramDistrubuida
         static UdpClient socket;
 
         static void OnUdpData(IAsyncResult result) {
-            // this is what had been passed into BeginReceive as the second parameter:
+
             UdpClient socket = result.AsyncState as UdpClient;
-            // points towards whoever had sent the message:
             IPEndPoint source = new IPEndPoint(0, 0);
-            // get the actual message and fill out the source:
             byte[] message = socket.EndReceive(result, ref source);
-            // do what you'd like with `message` here:
+
             Console.WriteLine("Got " + message.Length + " bytes from " + source);
 
-            String json = System.Text.Encoding.ASCII.GetString(message);
+            String json = Encoding.ASCII.GetString(message);
 
             model.Mensagem MensagemRecebida = JsonConvert.DeserializeObject<model.Mensagem>(json);
             Console.WriteLine(MensagemRecebida);
-            
+
+            using (StreamWriter sw = File.AppendText(logPath))
+                sw.WriteLine("RECEIVED>>> "+ json);
+
             socket.BeginReceive(new AsyncCallback(OnUdpData), socket);
         }
 
         static void OutUdpData( model.Mensagem mensagem) {
             socket.Client.SetSocketOption( SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-
             socket.BeginReceive(new AsyncCallback(OnUdpData), socket);
-            // sending data (for the sake of simplicity, back to ourselves):
+
             IPEndPoint target = new IPEndPoint(IPAddress.Parse(configuracao.IP), configuracao.Port);
 
-            // send a couple of sample messages:
             byte[] message = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(mensagem));
+
+            using (StreamWriter sw = File.AppendText(logPath))
+                sw.WriteLine("SENT>>> "+JsonConvert.SerializeObject(mensagem));
+
             socket.Send(message, message.Length, target);
+
         }
 
         static readonly string textFile = @"config/config.json";
-        static void Main(string[] args){
+        static readonly string logPath = @"log.txt";
 
+        static void Main(string[] args) {
+
+            if (!File.Exists(logPath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(logPath))
+                    sw.WriteLine(DateTime.Now);
+            }
             String caminhoArquivo = textFile;
             if (File.Exists(caminhoArquivo)){
 
@@ -52,7 +64,7 @@ namespace ProjetoChatProgramDistrubuida
                 configuracao = JsonConvert.DeserializeObject<model.Config>(File.ReadAllText(caminhoArquivo));
 
                 socket = new UdpClient(configuracao.Port);
-                while (true){
+                while (true) {
                     model.Mensagem mensagem = new model.Mensagem();
                     Console.WriteLine("Escreva uma mensagem");
                     mensagem.Message = Console.ReadLine();
@@ -60,6 +72,7 @@ namespace ProjetoChatProgramDistrubuida
                     Console.WriteLine("");
                 }
             }
+
         }
     }
 }
